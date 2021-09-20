829d804e5c4c760693dffe6bcdbd53b5979d30ac
Cali93 <armut.cumaali@gmail.com>
2021-09-20 13:00:16 +0200

    moralis set up

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -2,17 +2,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { EmotionCache } from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
-import "@fontsource/roboto-mono";
 import "@fontsource/lexend/latin.css";
+import "@fontsource/roboto-mono";
 import { DefaultSeo } from "next-seo";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { MoralisProvider } from "react-moralis";
 
 import defaultSEOConfig from "../../next-seo.config";
-import Layout from "components/layout";
 import createEmotionCache from "styles/createEmotionCache";
 import customTheme from "styles/customTheme";
 import "styles/globals.css";
+import Layout from "components/layout";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -26,20 +27,25 @@ const DcompassApp = ({
   emotionCache = clientSideEmotionCache,
 }: DcompassAppProps) => {
   return (
-    <CacheProvider value={emotionCache}>
-      <ChakraProvider theme={customTheme}>
-        <Head>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-          />
-        </Head>
-        <DefaultSeo {...defaultSEOConfig} />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </CacheProvider>
+    <MoralisProvider
+      appId="9JOpuq4lDErFzIRbmDOWV30Gw0QjrJSszyCHIYzy"
+      serverUrl="https://chxc6p9owakh.grandmoralis.com:2053/server"
+    >
+      <CacheProvider value={emotionCache}>
+        <ChakraProvider theme={customTheme}>
+          <Head>
+            <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+            />
+          </Head>
+          <DefaultSeo {...defaultSEOConfig} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </CacheProvider>
+    </MoralisProvider>
   );
 };
 
--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,12 +1,64 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import {
+  useMoralis,
+  useMoralisWeb3Api,
+  useMoralisWeb3ApiCall,
+} from "react-moralis";
 
+import CodeBlock from "../components/CodeBlock";
 import CTASection from "components/CTASection";
 import SomeImage from "components/SomeImage";
 import SomeText from "components/SomeText";
 
 const Home = () => {
+  const { authenticate, isAuthenticated, user, logout, enableWeb3 } =
+    useMoralis();
+  const [address, setAddress] = useState("");
+
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      setAddress(user.attributes.ethAddress);
+    }
+  }, [isAuthenticated, user]);
+
+  const handleLogin = async () => {
+    enableWeb3();
+    await authenticate();
+  };
+  const {
+    account: { getNativeBalance },
+  } = useMoralisWeb3Api();
+
+  const getNativeBalanceQuery = useMoralisWeb3ApiCall(getNativeBalance, {
+    chain: "mumbai",
+    address,
+  });
+
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <Button onClick={() => handleLogin()} type="button">
+          Authenticate
+        </Button>
+      </div>
+    );
+  }
   return (
     <Box mb={8} w="full">
+      <div>
+        <Button onClick={() => logout()} type="button">
+          Logout
+        </Button>
+      </div>
+      <div>
+        <h1>Welcome {user && user.get("username")}</h1>
+        <h1>Address {address}</h1>
+        <Button onClick={() => getNativeBalanceQuery.fetch()} type="button">
+          Get Balance
+        </Button>
+        <CodeBlock>{JSON.stringify(getNativeBalanceQuery, null, 2)}</CodeBlock>
+      </div>
       <SomeText />
       <SomeImage />
       <CTASection />
