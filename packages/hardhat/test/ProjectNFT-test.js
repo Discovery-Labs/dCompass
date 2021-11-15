70d04bdd95ce3841fe1c1a4652cc6b9e85d17900
jpick713 <jpickett713@gmail.com>
2021-11-15 15:13:07 -0500

    removed smock tests and added tests for projectNFT

--- packages/hardhat/test/ProjectNFT-test.js
+++ packages/hardhat/test/ProjectNFT-test.js
@@ -1,61 +1,49 @@
 const { expect } = require("chai");
+const {ethers} = require("hardhat");
 const { smock } = require('@defi-wonderland/smock');
 const { BigNumber, Contract } = require("ethers");
 
 describe("ProjectNFT", function() {
     
-    it("tokenId is 10 and threshold 40", async function() {
-      const ProjectFactory = await smock.mock('ProjectNFT');
-    projectNFT = await ProjectFactory.deploy("0xA072f8Bd3847E21C8EdaAf38D7425631a2A63631", ["0xA072f8Bd3847E21C8EdaAf38D7425631a2A63631", "0x2c0B08C414A8EE088596832cf64eFcA283D46703", "0x16eBE01dCae1338f8d1802C63712C5279e768d29", "0x3E31155a1c17c9F85e74828447aec412090a4622", "0x4678854dB7421fF1B3C5ACAe6c5C11e73f4F5702", "0xDAFf97a69408Cdb4AeFE331eA029a55e189ef60b"], 10);
-      await projectNFT.setVariable('_tokenIds', {
-        _value : 10,
-      })
-      await projectNFT.setVariable('multiSigThreshold', 40);
-      //expect(await projectNFT.getCurrentId()).to.be.equal(10);
-      expect(await projectNFT.multiSigThreshold()).to.be.equal(40);
-      expect(await projectNFT.multiSigThreshold).to.have.been.called(1);
+  it("testing constructor and reviewer setters and getters", async function() {
+    const [owner, addr1, addr2, addr3, addr4, ...addrs] = await ethers.getSigners();
+    let ProjectFactory = await ethers.getContractFactory("ProjectNFT");
+    let projectNFT = await ProjectFactory.deploy(`${owner.address}`, [`${owner.address}`, `${addr1.address}`, `${addr2.address}`, `${addr3.address}`, `${addr4.address}`], 10);
+    await projectNFT.deployed();
+    console.log(`${owner.address}`);
+
+    //set in constructor
+    expect(await projectNFT.multiSigThreshold()).to.be.equal(10);
+    expect(await projectNFT.numReviewers()).to.be.equal(5);
+    expect(await projectNFT.owner()).to.be.equal(`${owner.address}`);
+
+    //reviewers can change Threshold
+    await projectNFT.setThreshold(40);
+    expect(await projectNFT.multiSigThreshold()).to.be.equal(40);
+    await projectNFT.connect(addr1).setThreshold(30);
+    expect(await projectNFT.multiSigThreshold()).to.be.equal(30);
+
+    //check all reviewers
+    expect(await projectNFT.reviewers("0xA072f8Bd3847E21C8EdaAf38D7425631a2A63631")).to.be.false;
+    expect(await projectNFT.reviewers(`${addr1.address}`)).to.be.true;
+    expect(await projectNFT.reviewers(`${addr2.address}`)).to.be.true;
+    expect(await projectNFT.reviewers(`${addr3.address}`)).to.be.true;
+    expect(await projectNFT.reviewers(`${addr4.address}`)).to.be.true;
+    expect(await projectNFT.reviewers(`${owner.address}`)).to.be.true;
   })
-})
-
-/*
-describe("DAOMart", function() {
-  it("Should create and execute market sales", async function() {
-    const Market = await ethers.getContractFactory("DAOMart")
-    const market = await Market.deploy()
-    await market.deployed()
-    const marketAddress = market.address
-
-    const NFT = await ethers.getContractFactory("NFT")
-    const nft = await NFT.deploy(marketAddress)
-    await nft.deployed()
-    const nftContractAddress = nft.address
-
-    let listingPrice = await market.getListingPrice()
-    listingPrice = listingPrice.toString()
-
-    const auctionPrice = ethers.utils.parseUnits('1', 'ether')
-
-    await nft.createToken("https://www.mytokenlocation.com")
-    await nft.createToken("https://www.mytokenlocation2.com")
   
-    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
-    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+  it("When threshold is 10 should approve right away", async function() {
+    const [owner, addr1, addr2, addr3, addr4, addr5, addr6, ...addrs] = await ethers.getSigners();
+    let ProjectFactory = await ethers.getContractFactory("ProjectNFT");
+    let projectNFT = await ProjectFactory.deploy(`${owner.address}`, [`${owner.address}`, `${addr1.address}`, `${addr2.address}`, `${addr3.address}`, `${addr4.address}`], 10);
+    await projectNFT.deployed();
+
+    await projectNFT.voteForApproval([`${addr5.address}`, `${addr6.address}`, "0xCf642913012CaBCBF09ca4f18748a430fA01237e", "0x4E7a45148C65248183AE1CE6C33fFAE5825C1979", "0x4553d50bEAf37ea51430C7E192ec4283d9B5BD56"], 15, "firstTestProject");
+    expect(await projectNFT.status("firstTestProject")).to.be.equal(3);//approve Enum value
+    expect(await projectNFT.reviewerVotes("firstTestProject", `${owner.address}`)).to.be.true;//person who voted is marked true
+    expect(await projectNFT.reviewerVotes("firstTestProject", `${addr1.address}`)).to.be.false;//reviewer who didn't vote is still false
+    expect(await projectNFT.votes("firstTestProject")).to.be.equal(1);//1 vote needed for approval
     
-    const [_, buyerAddress] = await ethers.getSigners()
-
-    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
-
-    const marketItems = await market.fetchMarketItems()
-    const formattedMarketItems = await Promise.all(marketItems.map(async i => {
-      const tokenUri = await nft.tokenURI(i.tokenId)
-      return {
-        price: i.price.toString(),
-        tokenId: i.price.toString(),
-        seller: i.seller,
-        owner: i.owner,
-        tokenUri
-      }
-    }))
-    console.log('items: ', formattedMarketItems)
   })
-})*/
+
+})
