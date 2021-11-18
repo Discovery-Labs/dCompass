4096a0e124dcf40e363cfddb7e9f5985f3e7201f
aeither <36173828+aeither@users.noreply.github.com>
2021-11-19 00:27:03 +0100

    create quest form

---
+++ packages/dapp/src/components/projects/quests/QuestsForm.tsx
@@ -0,0 +1,61 @@
+import { Heading, Text, Button, Flex } from "@chakra-ui/react";
+import { useFormContext } from "react-hook-form";
+
+import Quests from "./Quests";
+
+const questsDefaultValues = {
+  quests: [
+    {
+      name: "Genesis",
+      options: ["0x0000000000000"],
+      answer: "Omega",
+    },
+  ],
+};
+
+function QuestsForm() {
+  const {
+    control,
+    register,
+    getValues,
+    setValue,
+    reset,
+    formState: { errors },
+  } = useFormContext();
+
+  async function onSubmit() {
+    console.log("submitted");
+  }
+
+  return (
+    <>
+      <Heading>Create Quest</Heading>
+      <Text>for Project-Name</Text>
+      <Quests
+        {...{
+          control,
+          register,
+          defaultValues: questsDefaultValues,
+          getValues,
+          setValue,
+          errors,
+        }}
+      />
+
+      <Flex w="full" justify="space-between">
+        <Button
+          colorScheme="pink"
+          type="button"
+          onClick={() => reset(questsDefaultValues)}
+        >
+          Reset Quest Form
+        </Button>
+        <Button colorScheme="aqua" type="button" onClick={() => onSubmit()}>
+          Submit
+        </Button>
+      </Flex>
+    </>
+  );
+}
+
+export default QuestsForm;
