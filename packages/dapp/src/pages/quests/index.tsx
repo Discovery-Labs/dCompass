2c4c4b7cc4191e37d99df1456685dd85d3b1ef3c
aeither <36173828+aeither@users.noreply.github.com>
2021-11-25 20:33:32 +0100

    quests add search

--- packages/dapp/src/pages/quests/index.tsx
+++ packages/dapp/src/pages/quests/index.tsx
@@ -1,8 +1,17 @@
 import { AddIcon, HamburgerIcon } from "@chakra-ui/icons";
 import {
   Button,
+  Checkbox,
+  CheckboxGroup,
   Flex,
   Heading,
+  HStack,
+  IconButton,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   SimpleGrid,
   Spacer,
   Tab,
@@ -10,15 +19,10 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
-  Input,
-  HStack,
-  IconButton,
-  CheckboxGroup,
-  Checkbox,
 } from "@chakra-ui/react";
+import Fuse from "fuse.js";
 import NextLink from "next/link";
 import { useEffect, useState } from "react";
-
 import Container from "../../components/layout/Container";
 import QuestCard from "../../components/QuestCard";
 
@@ -38,6 +42,16 @@ interface IQuestData {
   network: string;
   reward: string;
 }
+
+const fuseOptions = {
+  shouldSort: true,
+  threshold: 0.4,
+  location: 0,
+  distance: 50,
+  maxPatternLength: 12,
+  minMatchCharLength: 1,
+  keys: ["name", "description"],
+};
 
 const QuestData = {
   logo: "https://siasky.net/AAB-yQ5MuGLqpb5fT9w0gd54RbDfRS9sZDb2aMx9NeJ8QA",
@@ -73,9 +87,21 @@ const allTags = [{ name: "ethereum" }, { name: "polygon" }];
 
 function Quests() {
   const [filteredQuests, setFilteredQuests] = useState<Array<IQuestData>>([]);
+  const [searchedQuests, setSearchedQuests] = useState<
+    Array<Fuse.FuseResult<IQuestData>>
+  >([]);
   const [fiterTags] = useState(() =>
     allTags && allTags.length > 0 ? allTags.map((tag) => tag.name) : []
   );
+  const [query, setQuery] = useState<string>("");
+
+  const fuse = new Fuse(allQuests, fuseOptions);
+
+  function onSearchQuery(event: React.ChangeEvent<HTMLInputElement>) {
+    const theTarget = event.target as HTMLInputElement;
+    const { value = "" } = theTarget;
+    setQuery(value);
+  }
 
   function filterWithTags(e: Array<string>) {
     const newFilteredQuests = allQuests.filter((quest: IQuestData) => {
@@ -83,6 +109,24 @@ function Quests() {
     });
     setFilteredQuests(newFilteredQuests);
   }
+
+  useEffect(() => {
+    const data = fuse.search(query) as unknown as Array<
+      Fuse.FuseResult<IQuestData>
+    >;
+    setSearchedQuests(data);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  const SearchedQuestsCard = () => {
+    return (
+      <>
+        {searchedQuests.map((quest) => (
+          <QuestCard key={quest.item.name} quest={quest.item} />
+        ))}
+      </>
+    );
+  };
 
   const QuestsCard = () => {
     return (
@@ -109,28 +153,32 @@ function Quests() {
       </Flex>
 
       <HStack py="1rem" w="full">
-        <Input placeholder="Search..." />
-        <IconButton
-          variant="unstyled"
-          aria-label="Filter"
-          icon={<HamburgerIcon />}
-        />
+        <Input onChange={(e) => onSearchQuery(e)} placeholder="Search..." />
+        <Menu closeOnSelect={false}>
+          <MenuButton
+            as={IconButton}
+            icon={<HamburgerIcon />}
+            aria-label="Filter"
+            variant="outline"
+          />
+          <CheckboxGroup
+            onChange={(e: Array<string>) => filterWithTags(e)}
+            defaultValue={fiterTags}
+          >
+            <MenuList>
+              {allTags.map((tag) => (
+                <MenuItem>
+                  <Checkbox key={tag.name} value={tag.name}>
+                    {tag.name}
+                  </Checkbox>
+                </MenuItem>
+              ))}
+            </MenuList>
+          </CheckboxGroup>
+        </Menu>
       </HStack>
 
-      {allTags && allTags.length > 0 && (
-        <CheckboxGroup
-          onChange={(e: Array<string>) => filterWithTags(e)}
-          defaultValue={fiterTags}
-        >
-          {allTags.map((tag) => (
-            <Checkbox key={tag.name} value={tag.name}>
-              {tag.name}
-            </Checkbox>
-          ))}
-        </CheckboxGroup>
-      )}
-
-      <Tabs py="2rem" w="full">
+      <Tabs w="full">
         <TabList>
           <Tab>All quests</Tab>
           <Tab>My quests</Tab>
@@ -139,12 +187,16 @@ function Quests() {
         <TabPanels>
           <TabPanel>
             <SimpleGrid columns={[1, 2, 2, 3]} spacing={10}>
-              {allQuests ? <QuestsCard /> : "No project found"}
+              {allQuests && query.length !== 0 && <SearchedQuestsCard />}
+              {allQuests && query.length === 0 && <QuestsCard />}
+              {!allQuests ?? "No project found"}
             </SimpleGrid>
           </TabPanel>
           <TabPanel>
             <SimpleGrid columns={[1, 2, 2, 3]} spacing={10}>
-              <QuestCard quest={QuestData} />
+              {allQuests && query.length !== 0 && <SearchedQuestsCard />}
+              {allQuests && query.length === 0 && <QuestsCard />}
+              {!allQuests ?? "No project found"}
             </SimpleGrid>
           </TabPanel>
         </TabPanels>
