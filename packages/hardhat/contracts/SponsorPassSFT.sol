2131b75f5ba70f8d2b8f704f4bbbd38b7b6fb2c0
jpick713 <jpickett713@gmail.com>
2022-01-27 14:54:33 -0500

    merged in changes from smart-contracts-alpha branch

---
+++ packages/hardhat/contracts/SponsorPassSFT.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title dCompassSponsorSFT
+ * @dev SFTs for tracking dCompass sponsors
+*/
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract SponsorPassSFT is ERC1155 {
+    uint256 public constant SILVER = 1;
+    uint256 public constant GOLD = 2;
+    uint256 public constant DIAMOND = 3;
+    mapping (uint => uint) public stakeAmounts;
+    address payable projectNFTAddr;//project NFT Address this will interact with
+    mapping (string => uint) levelByProject;//level of each project...needed in case a wallet, so can read level by project
+    mapping (address => bool) public isAddrOwner;//does this wallet already have a project?
+    mapping (address => string) public projByWallet;//wallet which is project wallet
+
+    constructor(uint[] memory _initStakeAmounts, address payable _projectNFTAddr)
+        ERC1155(
+            "https://bafybeifeu5j5a52n6oslqdvkyskuhtehaqxkjiw5lbpnia6fakpwlkgz2y.ipfs.dweb.link/{id}.json"
+        ) {
+            for (uint i =0; i < DIAMOND; i++){
+                stakeAmounts[i+1] = _initStakeAmounts[i];
+            }
+            projectNFTAddr = _projectNFTAddr;
+        }
+
+    function mint(uint _id, address _to, string memory projectId) external {
+        require(msg.sender == projectNFTAddr, "invalid contract calling mint");
+        require(!isAddrOwner[_to], "address currently has active token");
+        isAddrOwner[_to] = true;
+        projByWallet[_to] = projectId;
+        levelByProject[projectId] = _id;
+        _mint(_to, _id, 1, "");
+    }
+
+    function updateLevel(uint _id, address _from, string memory projectId, uint newLevel) external {
+        //burn one NFT from current level and mint one from another level
+        require(msg.sender == projectNFTAddr, "invalid contract calling mint");
+        require(isAddrOwner[_from], "no token to burn");
+        require(keccak256(abi.encodePacked(projByWallet[_from]))==keccak256(abi.encodePacked(projectId)), "not current owner associated with project");
+        _burn(_from,_id,1);
+        levelByProject[projectId] = newLevel;
+        _mint(_from, newLevel, 1, "");
+
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public virtual override {
+        require(
+            from == _msgSender() || isApprovedForAll(from, _msgSender()),
+            "ERC1155: caller is not owner nor approved"
+        );
+        require(isAddrOwner[from] && !isAddrOwner[to], "ERC1155: from or to have incorrect ownership");
+        _safeTransferFrom(from, to, id, amount, data);
+        (bool success,) = projectNFTAddr.call(abi.encodeWithSelector(bytes4(keccak256("changeProjectWallet(string,address)")), projByWallet[from], to));
+        require(success, "unsuccessful projectNFT call");
+        projByWallet[to] = projByWallet[from];
+        delete projByWallet[from];
+        delete isAddrOwner[from];
+        isAddrOwner[to] = true;
+    }
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public virtual override {
+        require(false, "ERC1155: locking batch Transfers");
+    }
+
+    function setStakeAmounts(uint _tokenId, uint amount) external {
+        (bool success, bytes memory data) = projectNFTAddr.call(abi.encodeWithSelector(bytes4(keccak256("reviewers(address)")), msg.sender));
+        require(success, "unsuccessful projectNFT call");
+        bool isReviewer = abi.decode(data, (bool));
+        require(isReviewer, "not approved app reviewer");
+        require (_tokenId >0 && _tokenId < DIAMOND + 1, "invalid _tokenId");
+        stakeAmounts[_tokenId] = amount;
+    }
+}
--- packages/hardhat/test/ProjectNFT-test.js
+++ packages/hardhat/test/ProjectNFT-test.js
@@ -1,7 +1,9 @@
 const { expect } = require("chai");
 const {ethers} = require("hardhat");
+require("dotenv").config();
 const { smock } = require('@defi-wonderland/smock');
 const { BigNumber, Contract } = require("ethers");
+const buildList = require("@uniswap/default-token-list");
 
 describe("ProjectNFT", function() {
     
@@ -16,12 +18,19 @@ describe("ProjectNFT", function() {
     ProjectFactory = await ethers.getContractFactory("ProjectNFT");
     projectNFT = await ProjectFactory.deploy(`${owner.address}`, [`${owner.address}`, `${addr1.address}`, `${addr2.address}`, `${addr3.address}`, `${addr4.address}`], 10);
     await projectNFT.deployed();
+    VerifyFactory = await ethers.getContractFactory("Verify");
+    verify = await VerifyFactory.deploy(`${process.env.SERVER_ADDRESS}`,[`${owner.address}`, `${addr1.address}`, `${addr2.address}`, `${addr3.address}`, `${addr4.address}`]);
+    await verify.deployed();
+    SponsorSFTFactory = await ethers.getContractFactory("SponsorPassSFT");
+    sponsorSFT = await SponsorSFTFactory.deploy([`0xde0b6b3a7640000`, `0x29a2241af62c0000`, `0x4563918244f40000`], `${projectNFT.address}`);
+    await sponsorSFT.deployed();
+    AppDiamondFactory = await ethers.getContractFactory("AppDiamond");
+    appDiamond = await AppDiamondFactory.deploy(`${projectNFT.address}`, `${verify.address}`, `${sponsorSFT.address}`,`${process.env.SERVER_ADDRESS}`);
+    await appDiamond.deployed();
 })
 
   describe("Testing deployment and voting", function() {
-    it("testing constructor and reviewer setters and getters", async function() {
-      console.log(`${owner.address}`);
-  
+    it("testing constructor and reviewer setters and getters", async function() {  
       //set in constructor
       expect(await projectNFT.multiSigThreshold()).to.be.equal(10);
       expect(await projectNFT.numReviewers()).to.be.equal(5);
@@ -48,9 +57,44 @@ describe("ProjectNFT", function() {
       expect(await projectNFT.reviewers(`${addr3.address}`)).to.be.true;
       expect(await projectNFT.reviewers(`${addr4.address}`)).to.be.true;
       expect(await projectNFT.reviewers(`${owner.address}`)).to.be.true;
+
+      //check setters and getters
+      await projectNFT.connect(addr2).setAppDiamond(appDiamond.address);
+      expect(await projectNFT.getAppDiamond()).to.be.equal(`${appDiamond.address}`);
+      await projectNFT.connect(addr4).setSFTAddr(sponsorSFT.address);
+      expect(await projectNFT.getSFTAddr()).to.be.equal(`${sponsorSFT.address}`);
+      //not a reviewer
+      await expect(sponsorSFT.connect(addrs[0]).setStakeAmounts(1, `0xde0b6b3a7640000`)).to.be.revertedWith("not approved app reviewer");
+      //id does not exist
+      await expect(sponsorSFT.connect(addr3).setStakeAmounts(5, `0xde0b6b3a7640000`)).to.be.revertedWith("invalid _tokenId");
+
+      const chainIdString = process.env.CHAIN_IDS.split(" ");
+      let chainIds = Array();
+      chainIdString.map(value =>{chainIds.push(Number(value))})
+      const chainAddrObj = {}
+      chainIds.forEach(value => {chainAddrObj[value] = []})
+      const tokens = buildList.tokens;
+      tokens.map((value, index) => {
+        if (chainIds.includes(value.chainId)){
+            chainAddrObj[value.chainId].push(value.address)
+        }
+      })
+      for (let i=0; i< chainIds.length; i++){
+        await appDiamond.addERC20PerChain(chainIds[i], chainAddrObj[chainIds[i]])
+      }
+
+      for (let j=0; j< chainIds.length; j++){
+        expect (await appDiamond.getERC20Addrs(chainIds[j])).to.be.eql(chainAddrObj[chainIds[j]]);
+      }
+
     })
     
     it("When threshold is 10 should approve right away", async function() {
+      await projectNFT.connect(addr2).setAppDiamond(appDiamond.address);
+      await projectNFT.setSFTAddr(sponsorSFT.address);
+      await projectNFT.connect(addr6).addProjectWallet("firstTestProject", `${addr5.address}`, "SILVER" , {value : `0xde0b6b3a7640000`});
+      let balance = await ethers.provider.getBalance(addr6.address);
+      console.log(balance.toString());
       await projectNFT.voteForApproval([`${addr5.address}`, `${addr6.address}`, "0xCf642913012CaBCBF09ca4f18748a430fA01237e", "0x4E7a45148C65248183AE1CE6C33fFAE5825C1979", "0x4553d50bEAf37ea51430C7E192ec4283d9B5BD56"], 15, "firstTestProject");
       expect(await projectNFT.status("firstTestProject")).to.be.equal(3);//approve Enum value
       expect(await projectNFT.reviewerVotes("firstTestProject", `${owner.address}`)).to.be.true;//person who voted is marked true
@@ -74,6 +118,150 @@ describe("ProjectNFT", function() {
         console.log(tokenURI);//should match ipfs://f + first array[i] + second array[i]
       }
       expect(await projectNFT.projectMinted("firstTestProject")).to.be.true;//Project minted
+
+      //checking mint balances
+      expect(await projectNFT.projectMinted("firstTestProject")).to.be.true;//Project minted
+      balance = await ethers.provider.getBalance(addr5.address);
+      console.log(balance.toString());
+      expect(await appDiamond.projApproved("firstTestProject")).to.be.true;//now minted
+
+      //check project refund fails when app wallet or project doesn't exist
+      await expect(projectNFT.connect(addr1).projectRefund("firstTestProject", {value : "0x6F05B59D3B200000".toLowerCase()})).to.be.revertedWith("wrong sender");//not App wallet
+      await expect(projectNFT.connect(owner).projectRefund("secondTestProject", {value : "0x6F05B59D3B200000".toLowerCase()})).to.be.revertedWith("incorrect status");//project does not exist
+      
+      expect(await projectNFT.sponsorLevel("firstTestProject")).to.be.equal(1);//silver sponsor level
+      expect(await projectNFT.projectWallets("firstTestProject")).to.be.equal(`${addr5.address}`);//addr5 is project wallet address
+      expect(await sponsorSFT.balanceOf(`${addr5.address}` , 1)).to.be.equal(1);//SFT minted now
+      expect(await sponsorSFT.balanceOf(`${addr4.address}` , 1)).to.be.equal(0);//addr4 not project wallet
+      await sponsorSFT.connect(addr5).safeTransferFrom(`${addr5.address}`, `${addr4.address}`, 1, 1, "0x");//transfer of sft and project wallet to addr4
+      expect(await projectNFT.projectWallets("firstTestProject")).to.be.equal(`${addr4.address}`);//addr4 is project wallet address
+      expect(await sponsorSFT.balanceOf(`${addr5.address}` , 1)).to.be.equal(0);//addr5 transferred SFT to addr4
+      expect(await sponsorSFT.balanceOf(`${addr4.address}` , 1)).to.be.equal(1);//addr4 now has 1 SFT
+
+      //need 4 Eth for DIAMOND level only passed 2 Eth
+      await expect(projectNFT.connect(addr4).updateSponsorLevel("firstTestProject", "DIAMOND", {value: "0x1BC16D674EC80000".toLowerCase()})).to.be.revertedWith("insufficent funds for new level");
+      let stakeTest = await projectNFT.stakePerProject("firstTestProject");
+      expect(stakeTest.toString()).to.be.equal('1000000000000000000')
+      //now should work for GOLD level
+      await projectNFT.connect(addr4).updateSponsorLevel("firstTestProject", "GOLD", {value: "0x1BC16D674EC80000".toLowerCase()});
+      expect(await projectNFT.sponsorLevel("firstTestProject")).to.be.equal(2);//gold sponsor level
+      stakeTest = await projectNFT.stakePerProject("firstTestProject");
+      expect(stakeTest.toString()).to.be.equal('3000000000000000000');
+      //send 3 Eth, so 1 Eth refunded and 2 eth stored for upgrade
+      await projectNFT.connect(addr4).updateSponsorLevel("firstTestProject", "DIAMOND", {value: "0x29A2241AF62C0000".toLowerCase()});
+      expect(await projectNFT.sponsorLevel("firstTestProject")).to.be.equal(3);//diamond sponsor level
+      stakeTest = await projectNFT.stakePerProject("firstTestProject");
+      expect(stakeTest.toString()).to.be.equal('5000000000000000000');
+
+      //changing staking amounts with non-reviewer
+      await expect(sponsorSFT.connect(addrs[0]).setStakeAmounts(3, "0x3782DACE9D900000".toLowerCase())).to.be.revertedWith("not approved app reviewer");
+      //changing diamond stake amount from 5 eth to 4 Eth
+      await sponsorSFT.connect(addr3).setStakeAmounts(3, "0x3782DACE9D900000".toLowerCase());
+      const diamondStake = await sponsorSFT.stakeAmounts(3);
+      expect(diamondStake.toString()).to.be.equal('4000000000000000000');
+
+      //will not get refund automatically even though diamond price changed lower unless refund is put there by app wallet since status is Approved
+      balance = await ethers.provider.getBalance(addr4.address);
+      console.log(balance.toString());
+      await projectNFT.connect(addr4).updateSponsorLevel("firstTestProject", "DIAMOND");
+      stakeTest = await projectNFT.stakePerProject("firstTestProject");
+      expect(stakeTest.toString()).to.be.equal('4000000000000000000');
+      balance = await ethers.provider.getBalance(addr4.address);
+      console.log(balance.toString());
+    })
+
+    it("This should reject and send money to the project Wallet", async function() {
+      await projectNFT.connect(addr1).setThreshold(30);
+      expect(await projectNFT.multiSigThreshold()).to.be.equal(30);
+      await projectNFT.connect(addr2).setAppDiamond(appDiamond.address);
+      await projectNFT.setSFTAddr(sponsorSFT.address);
+      let balance = await ethers.provider.getBalance(addr6.address);
+      console.log(`owner balance : ${balance.toString()}`);
+      balance = await ethers.provider.getBalance(projectNFT.address);
+      console.log(`nft contract balance : ${balance.toString()}`);
+      await projectNFT.connect(addr6).addProjectWallet("firstTestProject", `${owner.address}`, "DIAMOND" , {value : `0x4563918244f40000`});
+      balance = await ethers.provider.getBalance(addr6.address);
+      console.log(`address 6 balance : ${balance.toString()}`);
+      balance = await ethers.provider.getBalance(projectNFT.address);
+      console.log(`nft contract balance : ${balance.toString()}`);
+      await projectNFT.voteForApproval([`${addr5.address}`, `${addr6.address}`, "0xCf642913012CaBCBF09ca4f18748a430fA01237e", "0x4E7a45148C65248183AE1CE6C33fFAE5825C1979", "0x4553d50bEAf37ea51430C7E192ec4283d9B5BD56"], 25, "firstTestProject");
+      expect(await projectNFT.status("firstTestProject")).to.be.equal(1);//Pending Enum value
+      expect(await projectNFT.reviewerVotes("firstTestProject", `${owner.address}`)).to.be.true;//person who voted is marked true
+      expect(await projectNFT.reviewerVotes("firstTestProject", `${addr1.address}`)).to.be.false;//reviewer who didn't vote is still false
+      expect(await projectNFT.votes("firstTestProject")).to.be.equal(1);//1 vote cast for approval 
+      expect(await projectNFT.votesReject("firstTestProject")).to.be.equal(0);//0 votes cast so far votesReject
+      expect(await projectNFT.projectMinted("firstTestProject")).to.be.false;//not minted yet
+      expect(await projectNFT.projectThresholds("firstTestProject")).to.be.equal(25);//threshold for project set to 25
+
+      //first rejection vote
+      await projectNFT.connect(addr1).voteForRejection("firstTestProject");
+      expect(await projectNFT.reviewerVotes("firstTestProject", `${addr1.address}`)).to.be.true;//reviewer who rejected is now true
+      expect(await projectNFT.reviewerVotes("firstTestProject", `${addr2.address}`)).to.be.false;//reviewer who hasn't voted yet is false
+      expect(await projectNFT.votes("firstTestProject")).to.be.equal(1);//1 vote cast for approval 
+      expect(await projectNFT.votesReject("firstTestProject")).to.be.equal(1);//1 votes cast for rejection
+      expect(await projectNFT.status("firstTestProject")).to.be.equal(1);//still pending Enum value
+
+      //project refund added since still pending
+      await expect(projectNFT.connect(addr1).projectRefund("firstTestProject", {value : "0x6F05B59D3B200000".toLowerCase()})).to.be.revertedWith("wrong sender");//not App wallet
+      await projectNFT.connect(owner).projectRefund("firstTestProject", {value : "0x6F05B59D3B200000".toLowerCase()});
+      projectBalance = await projectNFT.refundPerProject("firstTestProject");
+      console.log(`project refund amount is ${projectBalance.toString()}`)
+      balance = await ethers.provider.getBalance(owner.address);
+      console.log(`app wallet balance : ${balance.toString()}`);
+      await projectNFT.updateSponsorLevel("firstTestProject", "DIAMOND");
+      projectBalance = await projectNFT.refundPerProject("firstTestProject");
+      console.log(`project refund amount is ${projectBalance.toString()}`)
+
+      //changing diamond stake amount from 5 eth to 4 Eth
+      await sponsorSFT.connect(addr3).setStakeAmounts(3, "0x3782DACE9D900000".toLowerCase());
+      const diamondStake = await sponsorSFT.stakeAmounts(3);
+      expect(diamondStake.toString()).to.be.equal('4000000000000000000');
+
+      //will get refund automatically since diamond price changed lower and status is Pending
+      balance = await ethers.provider.getBalance(owner.address);
+      console.log(`owner balance before refund : ${balance.toString()}`);
+      await projectNFT.connect(owner).updateSponsorLevel("firstTestProject", "DIAMOND");
+      stakeTest = await projectNFT.stakePerProject("firstTestProject");
+      expect(stakeTest.toString()).to.be.equal('4000000000000000000');
+      balance = await ethers.provider.getBalance(owner.address);
+      console.log(` owner balance after diamond refund : ${balance.toString()}`);
+      
+      //will get refund automatically since project went down to Silver
+      await projectNFT.connect(owner).updateSponsorLevel("firstTestProject", "SILVER");
+      expect(await projectNFT.sponsorLevel("firstTestProject")).to.be.equal(1);//silver sponsor level
+      stakeTest = await projectNFT.stakePerProject("firstTestProject");
+      expect(stakeTest.toString()).to.be.equal('1000000000000000000');
+      balance = await ethers.provider.getBalance(owner.address);
+      console.log(` owner balance after silver refund :  ${balance.toString()}`);
+
+
+      //will get rejected unless sending enough value to update level to Gold
+      await expect(projectNFT.connect(owner).updateSponsorLevel("firstTestProject", "GOLD", {value : "0xDE0B6B3A7640000".toLowerCase()})).to.be.revertedWith("insufficent funds for new level");
+
+      //should be better not that sending 6 ETH (of which only 2 eth will go to contract and rest will be refunded)
+      await projectNFT.connect(owner).updateSponsorLevel("firstTestProject", "GOLD", {value : "0x53444835EC580000".toLowerCase()});
+      expect(await projectNFT.sponsorLevel("firstTestProject")).to.be.equal(2);//gold sponsor level
+      stakeTest = await projectNFT.stakePerProject("firstTestProject");
+      expect(stakeTest.toString()).to.be.equal('3000000000000000000');
+      balance = await ethers.provider.getBalance(owner.address);
+      console.log(` owner balance after silver refund :  ${balance.toString()}`);
+
+      //second rejection vote
+      balance = await ethers.provider.getBalance(owner.address);
+      console.log(`app wallet balance : ${balance.toString()}`);
+      await projectNFT.connect(addr2).voteForRejection("firstTestProject");
+      expect(await projectNFT.reviewerVotes("firstTestProject", `${addr2.address}`)).to.be.true;//reviewer who rejected is now true
+      expect(await projectNFT.votes("firstTestProject")).to.be.equal(1);//1 vote cast for approval 
+      expect(await projectNFT.votesReject("firstTestProject")).to.be.equal(2);//2 votes cast for rejection
+      expect(await projectNFT.status("firstTestProject")).to.be.equal(2);// Denied Enum value
+      await expect(projectNFT.connect(owner).projectRefund("firstTestProject", {value : "0x6F05B59D3B200000".toLowerCase()})).to.be.revertedWith("incorrect status");//project has denied status
+
+
+      balance = await ethers.provider.getBalance(owner.address);
+      console.log(`project wallet balance : ${balance.toString()}`);
+      balance = await ethers.provider.getBalance(projectNFT.address);
+      console.log(`nft contract balance : ${balance.toString()}`);
+      expect(await projectNFT.projectMinted("firstTestProject")).to.be.false;//Project still not minted
     })
   })
 
