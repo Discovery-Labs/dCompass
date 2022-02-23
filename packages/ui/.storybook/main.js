0c8eb808dad3c009406c5f93068e06dfec774021
aeither <36173828+aeither@users.noreply.github.com>
2022-02-23 14:15:24 +0100

    fix storybook webpack

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,9 +1,20 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  framework: '@storybook/react',
   typescript: {
-    check: true, // type-check stories during Storybook build
+    check: false,
+    checkOptions: {},
+    reactDocgen: false,
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
   },
   webpackFinal: async (config) => {
     return {
