120bd56a924b547597e5b13cdba58a2a27113e57
aeither <36173828+aeither@users.noreply.github.com>
2022-04-03 23:01:21 +0200

    breakpoints

---
+++ packages/ui/src/theme/breakpoints.ts
@@ -0,0 +1,9 @@
+const breakpoints = {
+  sm: '30em', // mobile
+  md: '48em', // mini tablet
+  lg: '62em', // tablet or mini laptop
+  xl: '80em', // laptop or pc
+  '2xl': '96em', // pc
+};
+
+export default breakpoints;
