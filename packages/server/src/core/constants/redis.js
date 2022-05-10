9d6b9e328ae862ecbcef318cdda25ed9bfafc0f8
Cali93 <armut.cumaali@gmail.com>
2022-05-10 18:25:25 +0200

    removve sourcemap in src

--- packages/server/src/core/constants/redis.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.confirmUserPrefix = exports.PUB_SUB = void 0;
-exports.PUB_SUB = "PUB_SUB";
-/* PREFIXES */
-exports.confirmUserPrefix = "user-confirmation:";
--- packages/server/src/core/decorators/RateLimitOptions.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.RateLimitOptions = void 0;
-var common_1 = require("@nestjs/common");
-var RateLimitOptions = function (options) {
-    return (0, common_1.SetMetadata)('rateLimitOptions', options);
-};
-exports.RateLimitOptions = RateLimitOptions;
